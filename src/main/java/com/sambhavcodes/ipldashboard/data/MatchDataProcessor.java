59d7a2cad0797c83ebccb251206bdf0832438751
mr-possible <sambhavd4@gmail.com>
2021-12-10 23:19:12 +0530

    Added match-data-processor file which processes the match-input. Added match.java file that would be our final model - the output.

---
+++ src/main/java/com/sambhavcodes/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,44 @@
+package com.sambhavcodes.ipldashboard.data;
+
+import com.sambhavcodes.ipldashboard.model.Match;
+import java.time.LocalDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+  private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+
+  @Override
+  public Match process(MatchInput matchInput) {
+    Match match = new Match();
+    match.setId(Long.parseLong(matchInput.getId()));
+    match.setCity(matchInput.getCity());
+    match.setDate(LocalDate.parse(matchInput.getDate()));
+    match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+    match.setVenue(matchInput.getVenue());
+    // Set team 1 and team 2 depending on innings order
+    String firstInningsTeam, secondInningsTeam = null;
+    if ("bat".equals(matchInput.getToss_decision())) {
+      firstInningsTeam = matchInput.getToss_winner();
+      secondInningsTeam =
+          matchInput.getToss_winner().equals(matchInput.getTeam1()) ? matchInput.getTeam2()
+              : matchInput.getTeam1();
+    } else {
+      secondInningsTeam = matchInput.getToss_winner();
+      firstInningsTeam =
+          matchInput.getToss_winner().equals(matchInput.getTeam1()) ? matchInput.getTeam2()
+              : matchInput.getTeam1();
+    }
+    match.setTeam1(firstInningsTeam);
+    match.setTeam2(secondInningsTeam);
+    match.setTossDecision(matchInput.getToss_decision());
+    match.setTossWinner(matchInput.getToss_winner());
+    match.setResult(matchInput.getResult());
+    match.setResultMargin(match.getResultMargin());
+    match.setUmpire1(matchInput.getUmpire1());
+    match.setUmpire2(matchInput.getUmpire2());
+    return match;
+  }
+}
