e90f3dc6bb68e5e7f94565c05c2426aafe95a654
mr-possible <sambhavd4@gmail.com>
2021-12-11 15:52:41 +0530

    Added basic route for Team Page.

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,22 +1,28 @@
 import React from "react";
 import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import {MatchDetailCard} from "../components/MatchDetailCard";
 import {MatchSmallCard} from "../components/MatchSmallCard";
 
 export const TeamPage = () => {
   const [team, setTeam] = useState({matches: []});
+  const {teamName} = useParams();
 
   useEffect(
       () => {
         const fetchMatches = async () => {
           const response = await fetch(
-              'http://localhost:8080/team/Delhi%20Capitals');
+              `http://localhost:8080/team/${teamName}`);
           const data = await response.json();
           setTeam(await data);
         };
         fetchMatches();
       }, []
   );
+
+  if (!team || !team.teamName) {
+    return <h1>Team not found !</h1>
+  }
   return (
       <div className="TeamPage">
         <h1>{team.teamName}</h1>
