36f551316f588540f78dc79eea73c50918dfdd37
mr-possible <sambhavd4@gmail.com>
2021-12-11 18:02:09 +0530

    Started with match page frontend.

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -2,12 +2,27 @@ import React from "react";
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {MatchDetailCard} from "../components/MatchDetailCard";
-import {MatchSmallCard} from "../components/MatchSmallCard";
 
 export const MatchPage = () => {
+  const [matches, setMatches] = useState([]);
+  const {teamName, year} = useParams();
+
+  useEffect(
+      () => {
+        const fetchMatches = async () => {
+          const response = await fetch(
+              `http://localhost:8080/teams/${teamName}/matches?year=${year}`);
+          const data = await response.json();
+          setMatches(data);
+        };
+        fetchMatches();
+      }, [] // call the hook when year changes
+  );
   return (
       <div className="MatchPage">
         <h1>Match Page</h1>
+        {matches.map(
+            match => <MatchDetailCard teamName={teamName} match={match}/>)}
       </div>
   );
 }
