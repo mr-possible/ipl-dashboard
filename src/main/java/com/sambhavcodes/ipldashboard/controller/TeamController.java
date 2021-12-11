7ccbb76a99abea5fc2e5667d6c7598b8cf7eeedf
mr-possible <sambhavd4@gmail.com>
2021-12-11 13:26:47 +0530

    Added team page.

--- src/main/java/com/sambhavcodes/ipldashboard/controller/TeamController.java
+++ src/main/java/com/sambhavcodes/ipldashboard/controller/TeamController.java
@@ -6,11 +6,13 @@ import com.sambhavcodes.ipldashboard.repository.MatchRepository;
 import com.sambhavcodes.ipldashboard.repository.TeamRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
   @Autowired
